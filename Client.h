905fcc748c22c0a495dda426b2da20f9ea05c8cf
Foantik <nasta3187@gmail.com>
2021-12-02 19:00:15 +0300

    Create Client.h

---
+++ Client.h
@@ -0,0 +1,20 @@
+#ifndef __Client_H__
+#define __Client_H__
+#include <iostream>
+#include <string>
+using namespace std;
+
+//фио клиента, номер заказа
+class Client {
+protected:
+	int Numb; // Номер заказа
+	string ClientFIO; // ФИО клиента
+public:
+	int getNumb(); // Получает номер заказа
+	string getClientFIO(); // Получает ФИО клиента
+	Client(string ClietnFIO);
+	~Client();
+
+};
+
+#endif // __Client_H__
